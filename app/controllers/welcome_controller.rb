fbe108a4deedbdfd1cd26f25cbb7d30bc75ddf6d
Elijah <Elijahtylerbrown@outlook.com>
2015-09-22 19:13:42 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,33 @@
+class WelcomeController < ApplicationController
+  def index
+  	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC )
+
+  	@states.sort!
+
+  if params[:city] != nil
+  	params[:city].gsub! " ", "_"
+
+  	response  = HTTParty.get("http://api.wunderground.com/api/93b7fc7670c38b15/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
+
+  	@location = response["current_observation"]["display_location"]["full"]
+  	@temp_f = response["current_observation"]["temp_f"]
+  	@temp_c = response["current_observation"]["temp_c"]
+  	@weather_icon = response["current_observation"]["icon_url"]
+  	@weather_words = response["current_observation"]["weather"]
+  	@forecast_link = response["current_observation"]["forecast_url"]
+  	@real_feel = response["current_observation"]["feelslike_f"]
+  end
+
+  def test
+  	response  = HTTParty.get("http://api.wunderground.com/api/93b7fc7670c38b15/conditions/q/NY/Brooklyn.json")
+
+  	@location = response["current_observation"]["display_location"]["full"]
+  	@temp_f = response["current_observation"]["temp_f"]
+  	@temp_c = response["current_observation"]["temp_c"]
+  	@weather_icon = response["current_observation"]["icon_url"]
+  	@weather_words = response["current_observation"]["weather"]
+  	@forecast_link = response["current_observation"]["forecast_url"]
+  	@real_feel = response["current_observation"]["feelslike_f"]
+  end
+end
+end
